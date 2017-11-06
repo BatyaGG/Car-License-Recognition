cb919ceed880a29d9d2dbbfd1a23add5a48174ad
BatyaGG <b.saduanov@gmail.com>
2017-11-07 02:11:03 +0600

    Several updates

--- Webcam.py
+++ Webcam.py
@@ -1,5 +1,4 @@
 import cv2
-from threading import Thread
 
 
 class Webcam:
@@ -7,14 +6,7 @@ class Webcam:
         self.video_capture = cv2.VideoCapture(0)
         self.current_frame = self.video_capture.read()[1]
 
-    # create thread for capturing images
-    def start(self):
-        Thread(target=self._update_frame, args=()).start()
-
-    def _update_frame(self):
-        while (True):
-            self.current_frame = self.video_capture.read()[1]
-
     # get the current frame
     def get_current_frame(self):
+        self.current_frame = self.video_capture.read()[1]
         return self.current_frame
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
+import math
 
 import cv2
 
@@ -6,11 +6,9 @@ from Webcam import Webcam
 from Detector import Detector
 from FrameToString import FrameToString
 
-
 webcam = Webcam()
 detector = Detector()
 frameToString = FrameToString()
-webcam.start()
 font = cv2.FONT_HERSHEY_SIMPLEX
 current_license = ''
 x = 152
@@ -20,12 +18,18 @@ while True:
     frame = webcam.get_current_frame()
     plate = detector.process_frame(frame)
     if plate is not None:
-        cv2.imshow('plate', plate)
+        try:
+            frame[frame.shape[0] - plate.shape[0] : frame.shape[0], 0 : plate.shape[1]] = plate
+        except:
+            rows0 = int(math.ceil(0.5 * (frame.shape[0] - plate.shape[0])))
+            rows1 = int(math.ceil(0.5 * frame.shape[0]))
+            cols0 = 0
+            cols1 = int(math.ceil(0.5 * plate.shape[1]))
+            frame[rows0 : rows1, cols0 : cols1] = cv2.resize(plate, ((cols1 - cols0), (rows1 - rows0)))
         tesser_out = frameToString.process(plate)
         if tesser_out is not None: current_license = tesser_out
     frame[y:y + 80, x:x + 335, :] = frame[y:y + 80, x:x + 335, :] / 2
     cv2.putText(frame, current_license, (160, 100), font, 2, (255, 255, 255), 2)
     cv2.imshow('vidos1', frame)
-    time.sleep(0.04)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
