0cca356342881db83f80a69b5ce921d14ea0cde3
BatyaGG <b.saduanov@gmail.com>
2017-11-07 04:24:43 +0600

    deleted several files

--- Webcam.py
+++ Webcam.py
@@ -6,7 +6,6 @@ class Webcam:
         self.video_capture = cv2.VideoCapture(0)
         self.current_frame = self.video_capture.read()[1]
 
-    # get the current frame
     def get_current_frame(self):
         self.current_frame = self.video_capture.read()[1]
         return self.current_frame
